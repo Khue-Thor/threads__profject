174b7ca35b1faa1be249538efc1b6bdc553c25eb
khuephamy <khuephumy19@gmail.com>
2023-08-06 11:48:38 -0500

    feat: sideleftbar

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,10 +1,12 @@
-import '../globals.css';
 import { ClerkProvider } from "@clerk/nextjs";
 import { Inter } from "next/font/google";
 
+
+import '../globals.css';
+
 export const metadata = {
   title: 'Threads',
-  description: 'A Next.js 12 Meta Threads Application'
+  description: 'A Next.js 13 Meta Threads Application'
 }
 
 const inter = Inter({ subsets: ["latin"] })
