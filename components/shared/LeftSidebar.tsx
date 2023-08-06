07472f0b7d83505937f2ec773e7765a8c8339ee2
khuephamy <khuephumy19@gmail.com>
2023-08-06 12:03:32 -0500

    feat: mobile bottom bar

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -1,8 +1,9 @@
 "use client"
 import { sidebarLinks } from "@/constants";
+import { SignOutButton, SignedIn } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
-import {usePathname, useRouter} from 'next/navigation'
+import { usePathname, useRouter } from 'next/navigation'
 
 function LeftSidebar() {
   const router = useRouter();
@@ -13,12 +14,22 @@ function LeftSidebar() {
         {sidebarLinks.map((link) => {
           const isActive = (pathname.includes(link.route) && link.route.length > 1) || pathname === link.route;
           return (
-            <Link href={link.route} key={link.label} className="leftsidebar_link">
+            <Link href={link.route} key={link.label} className={`leftsidebar_link ${isActive && 'bg-primary-500'}`}>
               <Image src={link.imgURL} alt={link.label} width={24} height={24} />
               <p className="text-light-1 max-lg:hidden">{link.label}</p>
             </Link>
           )
         })}
+      </div>
+      <div className="mt-10 px-6">
+        <SignedIn>
+          <SignOutButton signOutCallback={() => router.push('/sign-in')}>
+            <div className="flex cursor-pointer gap-4 p-4">
+              <Image src="/assets/logout.svg" alt="logout" width={24} height={24} />
+              <p className="text-light-2 max-lg:hidden">Logout</p>
+            </div>
+          </SignOutButton>
+        </SignedIn>
       </div>
     </section>
   )
